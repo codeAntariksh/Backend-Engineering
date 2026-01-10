ff6af62ca099e517061727ae69c79ec65009ac63
codeAntariksh <mailtoantariksh.work@gmail.com>
2026-01-10 14:51:54 +0530

    I Learnt Some Core Fundamentals of Express Where I Learnt About Routing, Handling Query Strings, Get, POST, etc Requests, Path Parameters, Many Express Base Functionalities with respect to Request Response Cycles using Express.js from our Server

---
+++ Express/index.js
@@ -0,0 +1,64 @@
+import express from 'express'
+const app = express();
+
+// console.dir(app);
+let port = 3000;
+
+app.listen(port, () => {
+    console.log("App Listening on Port " + port);
+});
+
+// app.use((req,res) => {
+//     console.log("Request Received");
+//     // res.send("This is a Basic Response!");
+//     // res.send({
+//     //     "id":23,
+//     //     "language":"JavaScript"
+//     // });
+//     let str = `<h1> The Reponse is: </h1>
+//     <ul> 
+//         <li> Object 1 </li>
+//         <li> Object 2 </li>
+//     </ul>`;
+//     res.send(str);
+//     // console.log(req);
+// });
+
+// app.get('/', (req, res) => {
+//     res.send("You Contacted Root Path");
+// });
+// app.post('/', (req, res) => {
+//     res.send("You Contacted Root Path and it's a POST Request");
+// });
+
+// app.get('/orange', (req, res) => {
+//     res.send("You Contacted Orange Path");
+// });
+
+// app.get('/apple', (req, res) => {
+//     res.send("You Contacted Apple Path");
+// });
+// app.post('/apple',(req,res) => {
+//     res.send("Hello I am Post");
+// });
+/* 404 handler — MUST be last */
+// app.use((req, res) => {
+//     res.status(404).send("This Page Does Not Exist");
+// });
+
+// PATH PARAMETERS
+
+// app.get('/:username/:id', (req, res) => {
+//     let { username, id } = req.params;
+//     // Combine both pieces of data into one response
+//     res.send(`<h1>Welcome to the page of @${username}</h1><p>User ID: ${id}</p>`);
+// });
+
+// app.get("/search",(req,res) => {
+//     let { q, color } = req.query;
+//     res.send("These are the Search Results of the Query:" + q + color);
+// })
+
+// app.get('/',(req,res) => {
+//     res.send("Hello, I am Root");
+// });
