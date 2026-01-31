cc754ad68afe184dad3bd9fb9dc3f0a0d0b43425
codeAntariksh <mailtoantariksh.work@gmail.com>
2026-02-01 00:40:25 +0530

    Handling Get and POST Requests

---
+++ Miscellaneous/Backend/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+const app = express();
+const PORT = 8080;
+// app.use(express.static('FrontEnd'));
+// app.use(express.)
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/register", (req, res) => {
+    let { user, password } = req.query;
+    res.send(`Standard GET Response. Username: ${user}, Password: ${password}`);
+});
+
+app.post("/register", (req, res) => {
+    // POST uses req.body (Data is hidden in the request body) 
+    console.log(req.body);
+    res.send(`Standard POST Response. Welcome!`);
+});
+
+app.listen(PORT,() => {
+    console.log(`Listening to PORT ${PORT}`);
+});
